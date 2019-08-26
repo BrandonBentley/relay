b9920846e1dabdae960b571204ee360c10d8bd9a
BrandonBentley <15896629+BrandonBentley@users.noreply.github.com>
2019-08-25 21:33:18 -0600

    added commandline args

--- main.go
+++ main.go
@@ -4,17 +4,26 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/BrandonBentley/relay/server"
 )
 
 func main() {
 	conf := getConfig()
+	if conf.BufferSize != 0 {
+		server.BufferSize = conf.BufferSize
+	}
+	if conf.StartingPort != 0 {
+		server.StartingPortNumber = conf.StartingPort
+	}
 	server.StartServer(conf.Port)
 }
 
 type config struct {
-	Port int
+	Port         int
+	BufferSize   int
+	StartingPort int
 }
 
 func getConfig() config {
@@ -28,8 +37,45 @@ func getConfig() config {
 		fmt.Printf("%v is not a valid port number", os.Args[1])
 		os.Exit(1)
 	}
-
-	return config{
+	conf := config{
 		Port: port,
 	}
+	if len(os.Args) > 3 {
+		skip := false
+		flagArgs := os.Args[2:]
+		length := len(flagArgs)
+		for i, v := range flagArgs {
+			if skip {
+				skip = false
+				continue
+			}
+			if v == "-b" || v == "--buffersize" {
+				if i+1 < length {
+					if strings.Contains(flagArgs[i+1], "-") {
+						fmt.Println("buffer size (int) missing")
+						os.Exit(1)
+					}
+					if conf.BufferSize, err = strconv.Atoi(flagArgs[i+1]); err != nil {
+						fmt.Println(flagArgs[i+1], " is an invalid buffer size")
+						os.Exit(1)
+					}
+				}
+				skip = true
+			} else if v == "-p" || v == "--startingport" {
+				if i+1 < length {
+					if strings.Contains(flagArgs[i+1], "-") {
+						fmt.Println("starting port (int) missing")
+						os.Exit(1)
+					}
+					if conf.StartingPort, err = strconv.Atoi(flagArgs[i+1]); err != nil {
+						fmt.Println(flagArgs[i+1], " is an invalid starting port")
+						os.Exit(1)
+					}
+				}
+				skip = true
+			}
+		}
+	}
+	fmt.Println(conf)
+	return conf
 }
