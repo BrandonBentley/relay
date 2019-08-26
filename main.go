e7baee814cb3f0d2a2f308e22b9e05c01fa8da91
BrandonBentley <15896629+BrandonBentley@users.noreply.github.com>
2019-08-26 11:04:53 -0600

    added -r cmdline flag and help output

--- main.go
+++ main.go
@@ -11,12 +11,7 @@ import (
 
 func main() {
 	conf := getConfig()
-	if conf.BufferSize != 0 {
-		server.BufferSize = conf.BufferSize
-	}
-	if conf.StartingPort != 0 {
-		server.StartingPortNumber = conf.StartingPort
-	}
+	setup(conf)
 
 	err := server.StartServer(conf.Port)
 	if err != nil {
@@ -25,22 +20,39 @@ func main() {
 	}
 }
 
+func setup(conf config) {
+	if conf.BufferSize != 0 {
+		server.BufferSize = conf.BufferSize
+	}
+	if conf.StartingPort != 0 {
+		server.StartingPortNumber = conf.StartingPort
+	}
+	if conf.RetryAmount != 0 {
+		server.RelayPortRetryAttempts = conf.RetryAmount
+	}
+}
+
 type config struct {
 	Port         int
 	BufferSize   int
 	StartingPort int
+	RetryAmount  int
 }
 
 func getConfig() config {
 	if len(os.Args) < 2 {
 		fmt.Println("Port number is required for execution")
+		printHelp(1)
 		os.Exit(1)
+	}
+	if os.Args[1] == "-h" || os.Args[1] == "help" || os.Args[1] == "--help" {
+		printHelp(0)
 	}
 	var port int
 	var err error
 	if port, err = strconv.Atoi(os.Args[1]); err != nil {
 		fmt.Printf("%v is not a valid port number", os.Args[1])
-		os.Exit(1)
+		printHelp(1)
 	}
 	conf := config{
 		Port: port,
@@ -58,11 +70,11 @@ func getConfig() config {
 				if i+1 < length {
 					if strings.Contains(flagArgs[i+1], "-") {
 						fmt.Println("buffer size (int) missing")
-						os.Exit(1)
+						printHelp(1)
 					}
 					if conf.BufferSize, err = strconv.Atoi(flagArgs[i+1]); err != nil {
 						fmt.Println(flagArgs[i+1], " is an invalid buffer size")
-						os.Exit(1)
+						printHelp(1)
 					}
 				}
 				skip = true
@@ -70,16 +82,44 @@ func getConfig() config {
 				if i+1 < length {
 					if strings.Contains(flagArgs[i+1], "-") {
 						fmt.Println("starting port (int) missing")
-						os.Exit(1)
+						printHelp(1)
 					}
 					if conf.StartingPort, err = strconv.Atoi(flagArgs[i+1]); err != nil {
 						fmt.Println(flagArgs[i+1], " is an invalid starting port")
-						os.Exit(1)
+						printHelp(1)
 					}
 				}
 				skip = true
+			} else if v == "-r" || v == "--retryattempts" {
+				if i+1 < length {
+					if strings.Contains(flagArgs[i+1], "-") {
+						fmt.Println("retryattempts (int) missing")
+						printHelp(1)
+					}
+					if conf.RetryAmount, err = strconv.Atoi(flagArgs[i+1]); err != nil {
+						fmt.Println(flagArgs[i+1], " is an invalid retry amount")
+						printHelp(1)
+					}
+				}
+				skip = true
+			} else {
+				fmt.Printf("%v is not a valid option", v)
+				printHelp(1)
 			}
 		}
 	}
 	return conf
+}
+
+func printHelp(exit int) {
+	fmt.Println("\nRelay Server")
+	fmt.Println("-------------------------------------------\n")
+	fmt.Println("usage: relay port {optional params}\n")
+	formatString := "%-30v %v\n"
+	fmt.Printf(formatString, "-b, --buffersize {size}", "set the size of each connection coupler (in bytes).")
+	fmt.Printf(formatString, "-p, --startingport {port}", "set first port to be used for relayed clients.")
+	fmt.Printf(formatString, "-r, --retryattempts {count}", "set the number of retry attempts allowed when finding")
+	fmt.Printf(formatString, "", " an available port for a new relay client.")
+
+	os.Exit(exit)
 }
